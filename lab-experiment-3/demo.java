8da5af12196e2f962853cf310250674a0e9d4c4e
Samuel Masih <sidhusamuel567@gmail.com>
2026-08-12 08:50:49 +0530

    lab-3

---
+++ lab-experiment-3/demo.java
@@ -0,0 +1,8 @@
+package abhi;
+public class demo {
+ public void sum(int num1, int num2) {
+  int result;
+  result = num1 + num2;
+  System.out.println("the sum of two numbers is:" + result);
+ }
+}
---
+++ lab-experiment-3/tester.java
@@ -0,0 +1,7 @@
+import abhi.demo;
+class tester extends demo {
+ public static void main(String args[]) {
+  tester obj = new tester();
+  obj.sum(10, 20);
+ }
+}
