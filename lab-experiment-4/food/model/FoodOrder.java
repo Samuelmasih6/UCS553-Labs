17492c0fe302a28b785c65f821974c6e9a85fa8a
Samuel Masih <sidhusamuel567@gmail.com>
2026-08-19 09:32:38 +0530

    bug fix

--- lab-experiment-4/food/model/FoodOrder.java
+++ lab-experiment-4/food/model/FoodOrder.java
@@ -1,61 +1,73 @@
 package food.model;
 
-public abstract class FoodOrder{
-	private int orderId;
-	private String customerName;
-	private double amount;
+public abstract class FoodOrder {
 
-	private static String restuarantName = "Food Express";
-	private static int orderCounter = 0;
+    private int orderId;
+    private String customerName;
+    private double amount;
 
-	public FoodOrder(int orderId, String customerName, double amount){
-		this.orderId = orderId;
-		this.customerName = customerName;
-		this.amount = amount;
-		orderCounter++;
-	}
+    // Static member
+    private static String restaurantName = "Food Express";
 
-	public int getOrderId(){
-		return orderId;
-	}
+    // Static object counter
+    private static int orderCounter = 0;
 
-	public void setOrderId(int orderId){
-		this.orderId = orderId;
-	}
+    // Constructor
+    public FoodOrder(int orderId, String customerName, double amount) {
+        this.orderId = orderId;
+        this.customerName = customerName;
+        this.amount = amount;
+        orderCounter++;
+    }
 
-	public String getCustomerName(){
-		return customerName;
-	}
+    // Getters and Setters
+    public int getOrderId() {
+        return orderId;
+    }
 
-	public void setCustomerName(String customerName){
-		this.customerName = customerName;
-	}
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
 
-	public double getAmount(){
-		return amount;
-	}
+    public String getCustomerName() {
+        return customerName;
+    }
 
-	public void setAmount(double amount){
-		this.amount = amount;
-	}
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
 
-	public static String getRestaurantName() {
-        	return restaurantName;
-	}
-	public static void setRestaurantName(String restaurantName) {
-        	FoodOrder.restaurantName = restaurantName;
-    	}
-	public static void displayTotalOrders() {
-        	System.out.println("Total Orders Created: " + orderCounter);
-	}
-	public abstract double calculateDeliveryCharge();
+    public double getAmount() {
+        return amount;
+    }
 
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
 
-	public void displayOrderDetails() {
-        	System.out.println("Restaurant   : " + restaurantName);
-        	System.out.println("Order ID     : " + orderId);
-        	System.out.println("Customer     : " + customerName);
-        	System.out.printf("Bill Amount  : Rs. %.2f%n", amount);
-	}
+    // Static getter
+    public static String getRestaurantName() {
+        return restaurantName;
+    }
+
+    // Static setter
+    public static void setRestaurantName(String restaurantName) {
+        FoodOrder.restaurantName = restaurantName;
+    }
+
+    // Static method
+    public static void displayTotalOrders() {
+        System.out.println("Total Orders Created: " + orderCounter);
+    }
+
+    // Abstract method
+    public abstract double calculateDeliveryCharge();
+
+    // Display order details
+    public void displayOrderDetails() {
+        System.out.println("Restaurant   : " + restaurantName);
+        System.out.println("Order ID     : " + orderId);
+        System.out.println("Customer     : " + customerName);
+        System.out.printf("Bill Amount  : Rs. %.2f%n", amount);
+    }
 }
-
